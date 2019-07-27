564a39a3a1ad787d155686154d31fe1a10f6eb55
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-07-27 09:43:57 -0400

    Write a function (countUniqueValues) to count the number of unique values in an array using multiple pointers

--- examples.js
+++ examples.js
@@ -93,5 +93,24 @@ sumZero = arr => {
 // sumZero([1, 2, 3]); // undefined
 
 // ---------------------------------------------------------------------------------------
-
+// Multiple Pointers - Count Unique Values
 // ---------------------------------------------------------------------------------------
+
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique vaules in the array. There can be negative numbers in the array, but it will always be sorted.
+
+countUniqueValues = (arr) => {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+};
+
+// countUniqueValues([1, 1, 1, 1, 1, 2]) // 2
+// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
+// countUniqueValues([-2, -1, -1, 0, 1]) // 4
+// countUniqueValues([]) // 0
