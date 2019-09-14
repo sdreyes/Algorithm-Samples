55a8f9e05ed973ce24ca27a2ab0c1244a94c2075
sdreyes <sheshechan90@gmail.com>
2019-09-14 11:39:49 -0400

    Refactor minSubarrayLength to use only one while loop

--- tests.js
+++ tests.js
@@ -202,6 +202,31 @@ let minSubarrayLen = (arr, num) => {
   return minCount;
 }
 
+
+let minnSubarrayLength = (arr, num) => {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minCount = Infinity;
+
+  while (start < arr.length) {
+    if (total < num && end < arr.length) {
+      total += arr[end];
+      end++;
+    }
+    else if (total >= sum) {
+      minCount = Math.min(minCount, end-start);
+      total -= arr[start];
+      start++;
+    }
+    else {
+      // if the total is still less than the target sum but the end is at the end is at the array length
+      break;
+    }
+  }
+  return minCount === Infinity ? 0 : minCount;
+}
+
 // console.log(minSubarrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4, 3] is the smallest subarray
 // console.log(minSubarrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5, 4] is the smallest subarray
 // console.log(minSubarrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is greater than 52
