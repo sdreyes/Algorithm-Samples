021512a642c6f4c19038065bb132e328f99ad3e4
sdreyes <sheshechan90@gmail.com>
2019-09-16 12:15:16 -0400

    Write recursive factorial function that takes in a number and returns the factorial of that number

--- recursion.js
+++ recursion.js
@@ -10,3 +10,15 @@ let power = (base, x) => {
 console.log(power(2, 0)) // 1
 console.log(power(2, 2)) // 4
 console.log(power(2, 4)) // 16
+
+// Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it; e.g. factorial 4 (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24. factorial zero (0!) is always 1.
+
+let factorial = num => {
+  if (num === 0) return 1;
+  return num * factorial(num-1);
+}
+
+console.log(factorial(1)) // 1
+console.log(factorial(2)) // 2
+console.log(factorial(4)) // 24
+console.log(factorial(7)) // 5040
