9c8f24b4579cc31c33df716ab5d421d0b7f3d902
sdreyes <sheshechan90@gmail.com>
2019-11-17 17:39:27 -0500

    Add selectionSort function

--- sorting.js
+++ sorting.js
@@ -2,7 +2,7 @@
 // Bubble Sort
 // ---------------------------------------------------------------------------------------
 
-let bubbleSort = (arr) => {
+let bubbleSort = arr => {
   for (let i = arr.length; i > 0; i--) {
     let done = true;
     for (let  j = 0; j < i - 1; j++) {
@@ -18,5 +18,30 @@ let bubbleSort = (arr) => {
   return arr;
 }
 
-console.log(bubbleSort([1,4,7,2,5,54,223,76,2,5,900,43]));
+// console.log(bubbleSort([1,4,7,2,5,54,223,76,2,5,900,43]));
 
+// ---------------------------------------------------------------------------------------
+// Bubble Sort
+// ---------------------------------------------------------------------------------------
+
+let selectionSort = arr => {
+  // Store the first element as the smallest value you've seen so far.
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i+1; j < arr.length; j++) {
+      // Compare this item to the next item in the array until you find a smaller number.
+      if ( arr[j] < arr[min]) min = j
+      // If a smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array.
+    }
+    // If the "minimum" is not the value (index) you initially began with, swap the two values.
+    if (min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  // Repeat this with the next element until the array is sorted.
+  return arr;
+}
+
+console.log(selectionSort([1,4,7,2,5,54,223,76,2,5,900,43]));
