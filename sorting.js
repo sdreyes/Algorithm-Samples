162ef519633d2e3169cc3bfa77ceaad104978755
sdreyes <sheshechan90@gmail.com>
2019-11-20 18:16:10 -0500

    Write function for insertion sort

--- sorting.js
+++ sorting.js
@@ -21,7 +21,7 @@ let bubbleSort = arr => {
 // console.log(bubbleSort([1,4,7,2,5,54,223,76,2,5,900,43]));
 
 // ---------------------------------------------------------------------------------------
-// Bubble Sort
+// Selection Sort
 // ---------------------------------------------------------------------------------------
 
 let selectionSort = arr => {
@@ -44,4 +44,24 @@ let selectionSort = arr => {
   return arr;
 }
 
-console.log(selectionSort([1,4,7,2,5,54,223,76,2,5,900,43]));
+console.log(selectionSort([2,4,7,1,5,54,223,76,2,5,900,43]));
+
+// ---------------------------------------------------------------------------------------
+// Insertion Sort
+// ---------------------------------------------------------------------------------------
+
+let insertionSort = arr => {
+  // Pick the second element in the array.
+  for (let i = 1; i < arr.length; i++) {
+    let currentNum = arr[i];
+    // Compare the second element with the one before it and swap if necessary.
+    for (var j = i-1; j >= 0 && currentNum < arr[j]; j--) {
+      // continue to the next element, and if it is in the incorrect order, iterate through the sorted portion (the left side) to place the element in the correct place.
+      arr[j+1] = arr[j];
+    };
+    arr[j+1] = currentNum;
+  };
+  return arr;
+}
+
+console.log(insertionSort([1,4,7,2,5,54,223,76,2,5,900,43]));
