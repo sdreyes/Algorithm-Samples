2c597d90c4114dc3cb25c7e691d4c18d1718c4d9
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-11-21 19:31:28 -0500

    Complete sliding window challenges

--- tests.js
+++ tests.js
@@ -158,7 +158,7 @@ let maxSubarraySum = (arr, len) => {
 // Time Complexity = O(n)
 // Space Complexity = O(1)
 
-let minSubarrayLen = (arr, num) => {
+let minSubarrayLenTemp = (arr, num) => {
   let minCount = Infinity;
   for (let i = 0; i < arr.length; i++) {
     let pointer = i;
@@ -175,6 +175,30 @@ let minSubarrayLen = (arr, num) => {
   return minCount;
 }
 
+let minSubarrayLen = (arr, num) => {
+  // THERE HAS TO BE A BETTER WAY TO DO THIS
+  let tempSum = 0;
+  let tempCount = 0;
+  let minCount = Infinity;
+  let i = 0;
+  let j = 0;
+  while (i < arr.length && minCount === Infinity) {
+    tempSum += arr[i];
+    tempCount += 1;
+    i++;
+    if (tempSum >= num) {
+      minCount = tempCount;
+    }
+    else if (i === arr.length) {
+      return 0;
+    }
+  }
+  while (j < arr.length) {
+    
+  }
+  return minCount;
+}
+
 console.log(minSubarrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4, 3] is the smallest subarray
 console.log(minSubarrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5, 4] is the smallest subarray
 console.log(minSubarrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is greater than 52
@@ -183,3 +207,34 @@ console.log(minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
 console.log(minSubarrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
 console.log(minSubarrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
 
+// ---------------------------------------------------------------------------------------
+// Sliding Window - minSubarrayLen
+// ---------------------------------------------------------------------------------------
+
+// Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters
+
+// Time Complexity - O(n)
+
+let findLongestSubstring = str => {
+  let longestSubstring = 0;
+  for (let letter of str) {
+    let stringLetters = {};
+    let currentSubstring = 0;
+    if (!stringLetters[letter]) {
+      stringLetters[letter] = 1;
+      currentSubstring += 1;
+      console.log(stringLetters);
+      console.log(currentSubstring);
+    }
+    if (currentSubstring > longestSubstring) longestSubstring = currentSubstring;
+  }
+  return longestSubstring;
+}
+
+// console.log(findLongestSubstring("")); // 0
+// console.log(findLongestSubstring("rithmschool")); // 7
+// console.log(findLongestSubstring("thisisawesome")); // 6
+// console.log(findLongestSubstring("thecatinthehat")); // 7
+// console.log(findLongestSubstring("bbbbbbb")); // 1
+// console.log(findLongestSubstring("longestsubstring")); // 8
+// console.log(findLongestSubstring("thisishowwedoit")); // 6
