618655928b264af986690b1284847224d69c08e4
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-12-15 09:46:06 -0500

    Complete push method for doubly linked list

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -18,12 +18,23 @@ class DoublyLinkedList {
   }
   push(val) {
     // Create a new node with the value passed to the function
+    let node = new Node(val);
     // If the head property is null, set the head and tail to be the newly created node
-    // If not, set the next property on the tail to be that node
-    // Set the previous property on the newly created node to be the tail
-    // Set the tail to be the newly created node
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      // If not, set the next property on the tail to be that node
+      this.tail.next = node;
+      // Set the previous property on the newly created node to be the tail
+      node.prev = this.tail;
+      // Set the tail to be the newly created node
+      this.tail = node;
+    }
     // Increment the length
-    // Return the double linked list
+    this.length++;
+    // Return the doubly linked list
+    return this;
   }
 
 }
